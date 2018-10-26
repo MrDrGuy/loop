55f4b281215c01ecfa80e4ece398b2235087ddc6
kbs5330 <kbs5330@psu.edu>
2018-10-26 10:50:36 -0400

    Start of login/modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,23 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { AcceptDialogComponent } from './accept-dialog/accept-dialog.component';
+import { RejectDialogComponent } from './reject-dialog/reject-dialog.component';
+import { UploadDialogComponent } from './upload-dialog/upload-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    MainScreenComponent
+    MainScreenComponent,
+    AcceptDialogComponent,
+    RejectDialogComponent,
+    UploadDialogComponent,
+    AcceptDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +33,10 @@ import { MatMenuModule } from '@angular/material/menu';
     MatExpansionModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatDialogModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { AcceptDialogComponent } from AcceptDialogComponent
 
 @Component({
   selector: 'app-main-screen',
