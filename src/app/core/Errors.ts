ec3a98a8a43a0cd93543b68f253bb4e22d5f1d8e
Joey Queppet <Joeq747@gmail.com>
2018-10-29 03:19:58 -0400

    Routing Functionality in app
    Adding the routing functionality, Routing module lists the current routes that can be navigated, nav-service controls the navigation

---
+++ src/app/core/Errors.ts
@@ -0,0 +1,13 @@
+
+export class appErrors {
+
+  authErrors1:string = 'No one is logged in. Please login before using the application.';
+
+
+  constructor(){}
+
+  getAuthErrors1(){return this.authErrors1};
+
+
+
+}
